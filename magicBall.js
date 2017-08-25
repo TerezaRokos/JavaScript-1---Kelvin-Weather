6c346bb43fd98f2d81a91e817887a4286faf71a0
Tereza R <30896452+TerezaRokos@users.noreply.github.com>
2017-08-25 20:13:18 +0200

    Update magicBall.js

--- magicBall.js
+++ magicBall.js
@@ -1,11 +1,8 @@
-let userName = 'Tessie';
-(userName === '') ? console.log('Hello!') : console.log('Hello, ' + userName + '!');
+var userQuestion = 'Are you rich?';
+var randomNumber = Math.floor(Math.random()*7);
+var eightBall = '';
 
-let userQuestion = '';
-let randomNumber = Math.floor(Math.random() * 7);
-let eightBall = '';
-
-switch (eightBall = randomNumber){
+switch (randomNumber) {
   case 0:
     eightBall = 'It is certain';
     break;
@@ -16,10 +13,10 @@ switch (eightBall = randomNumber){
     eightBall = 'Reply hazy try again';
     break;
   case 3:
-    eightBall = 'Cannot predict now';
+    eightBall = 'cannoct predict now';
     break;
   case 4:
-    eightBall = "Don't count on it";
+    eightBall = 'Don\'t count on it';
     break;
   case 5:
     eightBall = 'My sources say no';
@@ -29,7 +26,8 @@ switch (eightBall = randomNumber){
     break;
   case 7:
     eightBall = 'Signs point to yes';
-    break;
-                      }
+    break;      
+                    }
+
 console.log(userQuestion);
 console.log(eightBall);
