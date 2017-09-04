ff1535eb52ea062132da0a5afaf7e4f40fbbb05b
Tereza R <30896452+TerezaRokos@users.noreply.github.com>
2017-09-04 11:19:37 +0200

    Update passwordValidator.js

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,63 +1,58 @@
-function hasUppercase(input) {
-  for (var i = 0; i < input.length; i++) {
-    if (input[i] === input[i].toUpperCase()) {
-      return true;
-    }    
+function isPasswordValid(input) {
+ if(hasUppercase(input) === true) {
+   console.log('The password has an uppercase letter.');
+ } else {
+   console.log('Use an uppercase letter.');
+ }
+  if(hasLowercase(input) === true) {
+    console.log('The password has a lowercase letter.');
+  } else {
+    console.log('Use a lowercase letter.');
+  }
+  if(isLongEnough(input) === true) {
+    console.log('The password is long enough.');
+  } else {
+    console.log('Add some letters.');
+  }
+  if(hasSpecialCharacter(input) === true) {
+    console.log('The password has a special character.');
+  } else {
+    console.log('Add some special character.');
   }
 }
 
+function hasUppercase(input) {
+   for(var i=0; i<input.length; i++) {
+     if(input[i] === input[i].toUpperCase()) {
+       return true;
+     }
+   }
+ }
+
 function hasLowercase(input) {
-  for (var i = 0; i < input.length; i++) {
-    if (input[i] === input[i].toLowerCase()) {
+  for(var i=0; i<input.length; i++) {
+    if(input[i] === input[i].toLowerCase()) {
       return true;
     }
   }
 }
 
 function isLongEnough(input) {
-  for (var i = 0; i < input.length; i++) {
-    if (input.length >= 8) {
-      return true;
-    }
+  if(input.length >= 8) {
+    return true;
   }
-}
+ }
 
 function hasSpecialCharacter(input) {
   var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*'];
-  for (var i = 0; i < input.length; i++) {
-    for (var j = 0; j < specialCharacters.length; j++) {
-     if (input[i] === specialCharacters[j]) {
-       return true;
-     } 
+  for (var i=0; i<input.length; i++) {
+    for (var j=0; j<specialCharacters.length; j++) {
+      if(input[i] === specialCharacters[j]) {
+        return true;
+      }
     }
+    
   }
 }
 
-function isPasswordValid(input) {
-  if (hasUppercase(input)) {
-    console.log('The password has a capital.');
-  }
-  if (!hasUppercase(input)) {
-    console.log('The password needs a capital.');
-  }
-  if (hasLowercase(input)) {
-    console.log('The pasword has a small letter.');
-  }
-  if (!hasLowercase(input)) {
-    console.log('The password needs a small letter.');
-  }
-  if (isLongEnough(input)) {
-    console.log('The password is long enough.');
-  }
-  if (!isLongEnough(input)) {
-    console.log('The password needs more letters.');
-  }
-  if (hasSpecialCharacter(input)) {
-    console.log('The password has a special character.');
-  }
-  if (!hasSpecialCharacter(input)) {
-    console.log('The password needs a special character.');
-  }
-}
-
-isPasswordValid('validPassword');
+isPasswordValid('Heslo123*');
